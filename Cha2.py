3be94d48b5cca61202fb1ec634f506ccae8f23e7
ROMA <roma.petliuk.11@gmail.com>
2024-03-10 15:05:13 +0200

    added from Character1 import Character
    player1 = Character('Katyadyra')
    player1.show_info()
    player2 = Character('roma', damage=50)
    print(player2)
    
    while player1.hp == 0:
        p1_damage = player2.attack(player1)
        print(f'{player2.name} attack {player1.name} '
            f'1 наніс {p1_damage}  шкоди')
        print(player2, player1, sep='\n')

--- Cha2.py
+++ Cha2.py
@@ -6,10 +6,10 @@ player2 = Character('roma', damage=50)
 print(player2)
 
 while True:
-    if is_alive. >= 0:
-    p1_damage = player2.attack(player1)
-    print(f'{player2.name} attack {player1.name} '
-        f'1 наніс {p1_damage}  шкоди')
-    print(player2, player1, sep='\n')
+    if player1.is_alive() > 0:
+        p1_damage = player2.attack(player1)
+        print(f'{player2.name} attack {player1.name} '
+            f'1 наніс {p1_damage}  шкоди')
+        print(player2, player1, sep='\n')
 
 
