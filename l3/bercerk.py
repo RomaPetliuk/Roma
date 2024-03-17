a8411d30a8be9e8c4bff83bed9cd55e9fb3690fd
ROMA <roma.petliuk.11@gmail.com>
2024-03-17 15:38:46 +0200

    added

--- l3/bercerk.py
+++ l3/bercerk.py
@@ -1,11 +1,12 @@
-from l2.Character1 import Character
-class Berserk(Character):
-    def __init__(self, name, health = 100, damage = 1, defence = 0):
+from l2.character import Character
+
+class berserk(Character):
+    def __init__(self, name, health=100, damage=1, defence=0):
         super().__init__(name, health, damage, defence)
         self.max_health = health
 
     def attack(self, target):
         additional_damage = (1 - self.health / self.max_health) * self.damage
         return target.take_damage(
-            self.damage + additional_damage
+            round(self.damage + additional_damage)
         )
